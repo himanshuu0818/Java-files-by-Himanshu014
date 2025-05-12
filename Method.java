eb174bd3723b24b42742baac936fce9787b79b96
himanshuu0818 <singhhimanshu0818@gmail.com>
2025-05-12 22:39:10 +0530

    Add files via upload

---
+++ Method.java
@@ -0,0 +1,16 @@
+                //  Name -> Himanshu
+               //   Roll. No. -> 24/SCA/BCA/014
+
+public class Method {
+    void displayMessage() {
+        System.out.println("This is a method without arguments!");
+    }
+
+    public static void main(String[] args) {
+        // Create an object of the class
+        Method example = new Method();
+
+        // Call the method
+        example.displayMessage();
+    }
+}
